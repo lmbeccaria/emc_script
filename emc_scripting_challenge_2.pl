c4024b4b8ef0880de296d34658af0c687dc768a8
lmbeccaria <luis.m.beccaria@gmail.com>
2013-12-19 08:01:21 -0500

    Added total spent per customer, total spent per cust per category

--- emc_scripting_challenge_2.pl
+++ emc_scripting_challenge_2.pl
@@ -16,10 +16,20 @@ sub main
 
   my @customers_data =  &find_all_customer_data('input.txt') ;
 
-  #foreach my $n (@customers_data) { print "$n->{name}: $n->{amount}\n"; }
+  #foreach my $n (@customers_data) { message("$n->{name}: $n->{amount}"); }
 
   my @customers_totals = &total_spent_per_customer(@customers_data);
-  foreach my $n (@customers_totals) { print "$n->{name}: $n->{amount}\n"; }
+  
+  message("\nThese are the totals per customer:") ;
+  foreach my $n (@customers_totals) { message ("$n->{name}: $n->{amount}"); }
+  
+  my @customers_per_category = &total_spent_per_categoy(@customers_data);
+  
+  message("\nThese are the totals per customer per category:") ;
+  foreach my $n (@customers_per_category) { 
+     message("Name: $n->{name} ---- Groceries: $n->{groceries} Entretaiment: $n->{entertainment} Fuel: $n->{fuel}"); 
+  }
+  
   #Print Reports
 #  &print_report('input.txt', 'output.txt');
 
@@ -49,16 +59,10 @@ sub find_all_customer_data
   while(my $line = $filehandle->getline)
   {
     my ($name, $category, $str_amount) = ($line =~ m/(.*?)\|(.*?)\|(.*?)$/);
-    #chomp($str_amount);
-    my $amount;
-    if ( $str_amount =~ s/\$(\d+).*/$1/g) { $amount = int($1) }
-
-    
-    #my ($exists) = grep { $customers[$_] =~ /^$name$/ } 0..$#customers; 
-
-    #my ($exists) = $_ = grep( /^$name$/, $customers[$index]{name} );
-
-    #($exisits) = $_ = ($customers[$index]{'name'} eq $name)
+    chomp($name);
+    chomp($category);
+    chomp($str_amount);
+    my ($amount) = ($str_amount =~ m/\$(.*)?$/) ;
 
     $customers[$index]{name} = $name ;
     $customers[$index]{category} = $category ;
@@ -71,42 +75,98 @@ sub find_all_customer_data
 
 sub total_spent_per_customer
 {
-    my (@customers_data) = @_;
+    my (@data) = @_;
     my @customers_totals ;
+    my $customer = 0;
+    my $index = 0;
+    my $exists =0;
 
-    my $name = $customers_data
+    foreach my $entry (@data)
+    {
+      $exists = $_ = grep { $customers_totals[$_]{name} =~ /^$entry->{name}$/ } 0..$#customers_totals;
+        
+      if ($exists)
+      {
+         # Find the index in @customers_totals  where the customer name matches
+         for(my $i=0; $i < scalar(@customers_totals); $i++ )
+         {
+            if ($entry->{name} eq $customers_totals[$i]{name}) { $index = $i; last;}
+         }
+         #print "found index: $index \n" ;
+         $customers_totals[$index]{amount} += $entry->{amount} ;   
+         #print "This customer exists: $customers_totals[$index]{name}: $customers_totals[$index]{amount} : cust ID: [$customer] : Exists ID: [$index]\n";
+      } 
+      else {
+        $customers_totals[$customer]{name} = $entry->{name} ;   
+        $customers_totals[$customer]{amount} = $entry->{amount} ; 
 
-#    #print "name: $name -- amount: $amount||||\n";
-#
-#    if ($customers[$index]{name} eq $name)
-#    
-#    #if ( $exists )
-#    #if ( (defined $customers[$index]{name}) && ($name ~~ $customers[$index]{name}) )
-#    { 
-#        #print "Index: $index -- $customers[$index]{name} \n";
-#          #my ($exists) = grep { $customers[%_] =~ /^$name$/ } 0..$#customers;   
-#          print "customer exists\n";
-#        $customers[$index]->{amount} += $amount;
-#  
-#     } else {
-#        $customers[$index]->{name} = $name;
-#        $customers[$index]->{amount} = $amount;
-#
-#        #    print "$name : $customers[$index]->{name} : $customers[$index]->{amount} \n"
-#     }
-#    
-#    #if ($customers[$index]->{name} eq $name))
-#     #{
-#         #push(@customers, $name);
-#         # %customers{$name} = $amount;
-#     #}
-#     #print $name . "\n";
-#     $index++;
-#  
-#  #foreach my $n (@customers) { print "$n->{name}: $n->{amount}\n"; }
-#
+        #print "This customere NOT exists: $customers_totals[$customer]{name}: $customers_totals[$customer]{amount}  : cust ID: [$customer] : Exists ID: [$index]\n";
+        $customer ++ ;
+      }
+    }
     return @customers_totals;
 }
+
+sub total_spent_per_categoy
+{
+    my (@data) = @_;
+    my @customers_totals ;
+    my $customer = 0;
+    my $index = 0;
+    my $name_exists =0;
+    my $category;
+    
+    foreach my $entry (@data)
+    {
+      my $name = $entry->{name};
+      my $category = $entry->{category} ; 
+      my $amount = $entry->{amount} ; 
+
+      $name_exists = $_ = grep { $customers_totals[$_]{name} =~ /^$name$/ } 0..$#customers_totals;
+      if ($name_exists)
+      {
+         # Find the index in @customers_totals  where the customer's name matches
+         for(my $i=0; $i < scalar(@customers_totals); $i++ )
+         {
+            if ($name eq $customers_totals[$i]{name}) { $index = $i; last;}
+         }
+         #message("*********************\nfound index: $index Name: $name Cat: $category Amount: $amount\n") ;
+        
+         #message("Before Name: $customers_totals[$index]{name} Categ: $customers_totals[$index]{$category} Amount: $customers_totals[$index]{amount}\n"); 
+
+         # Check if category element already in the @customeres_totals.         
+        if (( $name eq $customers_totals[$index]{name}) && ( defined $customers_totals[$index]{$category}) )
+        {
+            #  message("---------------------------\nCustomer name and category exisits:\n");
+           $customers_totals[$index]{$category} += $amount ; 
+
+           # message("Name: $customers_totals[$index]{name} Categ: $customers_totals[$index]{$category} \n"); 
+           # message("cust ID: [$customer] : Exists ID: [$index]\n");
+        
+        } else { # If category not in the hash key yet add it.
+
+            # message("------------------------------\nCustomer name exists but NOT category:\n") ;
+
+           $customers_totals[$index]{$category} = $amount ; 
+         
+           #message("Name: $customers_totals[$index]{name} Categ: $customers_totals[$index]{$category}\n"); 
+           # message("cust ID: [$customer] : Exists ID: [$index]\n");
+        } 
+     } 
+      else {
+          #message("-------------------------------\nCustomer exisit NOT:\n");
+        $customers_totals[$customer]{name} = $name ;   
+        $customers_totals[$customer]{$category} = $amount ; 
+
+        #message("Name: $customers_totals[$customer]{name} Categ: $customers_totals[$customer]{$category}\n");
+        # message("cust ID: [$customer] : Exists ID: [$index]\n");
+        
+         $customer ++ ;
+      }
+    }
+    return @customers_totals;
+}
+
 
 sub print_report
 {
